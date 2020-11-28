cd9784a956c00712b533fc75ce2339472b226474
Ntakeda <muzat.yakuzajeep@gmail.com>
2020-11-28 14:05:27 +0900

    javaArray問題回答

--- Q06/Main.java
+++ Q06/Main.java
@@ -2,10 +2,15 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 		System.out.print("単語を入れてね>");
-		String word=new Scanner(System.in).next();
+		String word=new Scanner(System.in).nextLine();
 		//文字数をもとにchar配列を作成(配列変数名chars)
-		for(int i=0;i<chars.length;i++){
+		char[] chars=word.toCharArray();
+		//System.out.println(Arrays.toString(chars));
+		for(int i=0;i<chars.length/2;i++){
 			//配列にお尻の文字から詰めていく処理
+			char temp=chars[i];
+			chars[i]=chars[chars.length-i-1];
+			chars[chars.length-i-1]=temp;
 		}
 		System.out.println(Arrays.toString(chars));
 	}
